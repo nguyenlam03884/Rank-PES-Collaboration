dd1f2ea846576eeccc0c8c3fce071b221429cef1
nguyenlam03884 <nguyenlam03884@gmail.com>
2026-07-14 01:24:37 +0700

    Delete docs directory

--- docs/update_rooms_v1_7_6.sql
+++
@@ -1,8 +0,0 @@
--- PES 2026 V1.7.6: cho phép phòng mở không có khách.
--- Chạy một lần trong Supabase SQL Editor trước khi deploy code mới.
-
-alter table public.match_rooms
-    alter column guest_user_id drop not null;
-
-comment on column public.match_rooms.guest_user_id is
-    'Có thể NULL khi chủ phòng tạo phòng mở hoặc khách đã rời trước khi random đội.';
